cc78174d621a1b3dca035fd00c1ca6b142d3fa75
kevinlam825 <kevin.lam825@gmail.com>
2018-02-09 16:22:27 -0800

    fixed syntax and added testing

--- Homework/HW1/HW1.js
+++ Homework/HW1/HW1.js
@@ -2,48 +2,67 @@
 
 //Question 1
 function upperCase(str) {
-    let result = '';
+    let result = ''
     for (i = 0; i < str.length; i++) {
         if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
-            let temp = str.charCodeAt(i) - 32;
-            result += String.fromCharCode(temp);
-        } else result += str.charAt(i);
+            let temp = str.charCodeAt(i) - 32
+            result += String.fromCharCode(temp)
+        } else result += str[i]
     }
-    console.log(result);
+    return result
 }
 
 function lowerCase(str) {
-    let result = '';
+    let result = ''
     for (i = 0; i < str.length; i++) {
         if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
-            let temp = str.charCodeAt(i) + 32;
-            result += String.fromCharCode(temp);
-        } else result += str.charAt(i);
+            let temp = str.charCodeAt(i) + 32
+            result += String.fromCharCode(temp)
+        } else result += str[i]
     }
-    console.log(result);
+    return result
 }
 
 
 
 function alternatingCase(str) {
-    let result = '';
-    let temp;
+    let result = ''
+    let temp
     for (i = 0; i < str.length; i++) {
         if (i % 2 == 0) {
             if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
-                temp = str.charCodeAt(i) + 32;
-                result += String.fromCharCode(temp);
-            } else result += str.charAt(i);
+                temp = str.charCodeAt(i) + 32
+                result += String.fromCharCode(temp)
+            } else result += str[i]
         } else {
             if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
-                temp = str.charCodeAt(i) - 32;
-                result += String.fromCharCode(temp);
-            } else result += str.charAt(i);
+                temp = str.charCodeAt(i) - 32
+                result += String.fromCharCode(temp)
+            } else result += str[i]
         }
     }
-    console.log(result);
+    return result
 }
 
 function inverseCase(str) {
 
 }
+
+
+
+function runStringFunctions() {
+    let str = 'I watched the storm, so beautiful yet terrific. The face of the moon was in shadow.'
+
+    let unconditionallyCapitalized = ['I', 'Moon', 'Shadow']
+    let lowercaseWords = ['the', 'of', 'in', 'an']
+
+    console.log('upperCase: ', upperCase(str))
+    console.log('lowerCase: ', lowerCase(str))
+        // console.log('sentenceCase: ', sentenceCase(str, unconditionallyCapitalized))
+        // console.log('capitalizedCase: ', capitalizedCase(str))
+    console.log('alternatingCase: ', alternatingCase(str))
+        //console.log('titleCase: ', titleCase(str, lowercaseWords))
+        //console.log('inverseCase: ', inverseCase(str))
+}
+
+runStringFunctions();
