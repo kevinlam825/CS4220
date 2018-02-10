c98bd5db7ba7b470944012e54d7901b9ba07eaf7
kevinlam825 <kevin.lam825@gmail.com>
2018-02-09 17:01:32 -0800

    finished capalizedCase

--- Homework/HW1/HW1.js
+++ Homework/HW1/HW1.js
@@ -23,7 +23,18 @@ function lowerCase(str) {
     return result
 }
 
-
+function capitalizedCase(str) {
+    let result = ''
+    for (i = 0; i < str.length; i++) {
+        if (str[i - 1] == ' ' || i == 0) {
+            if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+                let temp = str.charCodeAt(i) - 32
+                result += String.fromCharCode(temp)
+            } else result += str[i]
+        } else result += str[i]
+    }
+    return result
+}
 
 function alternatingCase(str) {
     let result = ''
@@ -45,9 +56,11 @@ function alternatingCase(str) {
 }
 
 function inverseCase(str) {
+    let result = ''
+    for (i = 0; i < str.length; i++) {
 
+    }
 }
-
 
 
 function runStringFunctions() {
@@ -59,10 +72,10 @@ function runStringFunctions() {
     console.log('upperCase: ', upperCase(str))
     console.log('lowerCase: ', lowerCase(str))
         // console.log('sentenceCase: ', sentenceCase(str, unconditionallyCapitalized))
-        // console.log('capitalizedCase: ', capitalizedCase(str))
+    console.log('capitalizedCase: ', capitalizedCase(str))
     console.log('alternatingCase: ', alternatingCase(str))
         //console.log('titleCase: ', titleCase(str, lowercaseWords))
         //console.log('inverseCase: ', inverseCase(str))
 }
 
-runStringFunctions();
+runStringFunctions()
