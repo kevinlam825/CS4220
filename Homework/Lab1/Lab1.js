1607fcbf5028dcf1e0372b64b94684cd0d780e70
kevinlam825 <kevin.lam825@gmail.com>
2018-02-22 19:59:01 -0800

    finished assignment

--- Homework/Lab1/Lab1.js
+++ Homework/Lab1/Lab1.js
@@ -35,4 +35,45 @@ printTimes = (sample) => {
 
 printTimes(sample)
 
+
 //Section 2
+let resultObject = {
+    'success': [],
+    'error': []
+}
+
+checkSite = (site) => {
+    return new Promise((resolve, reject) => {
+        http.get(site, (res) => { //Make request
+            if (res.statusCode >= 200 && res.statusCode <= 399) resolve({ 'success': site }) //Check status codes & assigns corresponding values
+            if (res.statusCode >= 400 && res.statusCode <= 599) resolve({ 'error': site })
+        }).on('error', (err) => {
+            reject(err)
+        })
+    })
+}
+
+print = (sites) => {
+    const promises = sites.map((site) => {
+        return checkSite(site)
+    })
+
+    Promise.all(promises)
+        .then((results) => {
+            results.forEach((element) => {
+                if (element.success != null) resultObject.success.push(element.success) //Check if item was successful, if so add to success key
+                if (element.error != null) resultObject.error.push(element.error) //Check if item resulted in error, if so add to error key
+
+                if (resultObject.success.length + resultObject.error.length == sample.length) console.log(resultObject) //Print when all items have been added to resultObject
+            })
+        })
+        .catch(error => {
+            console.log('error', error)
+        })
+
+}
+
+setTimeout(() => {
+    console.log()
+    print(sample)
+}, 500)
