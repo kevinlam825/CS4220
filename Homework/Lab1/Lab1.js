0dce3d23eac2bf69ff636e93f90e5a1b552acfb9
kevinlam825 <kevin.lam825@gmail.com>
2018-02-22 18:11:54 -0800

    finished problem 1

--- Homework/Lab1/Lab1.js
+++ Homework/Lab1/Lab1.js
@@ -10,13 +10,29 @@ const sample = [
     'http://google.com/nothing'
 ]
 
-const getTimes = (argument, callback) => {
-    let startTime = new Date().getTime()
-
-    let endTime = new Date().getTime()
-    let finalTime = endTime - startTime
-
-    console.log(finalTime)
+getTimes = (argument, callback) => {
+    let startTime = new Date().getTime() //Get start time
+    http.get(argument, (res) => { //Make get request
+        callback({
+            'url': argument, //Set url
+            'time': new Date().getTime() - startTime //Set response time
+        })
+    }).on('error', (err) => {
+        console.log('Error: ' + err)
+    })
 }
 
-getTimes('a', 'b')
+printTimes = (sample) => {
+    sample.forEach(element => {
+        getTimes(element, (error, result) => {
+            if (error)
+                console.log(error)
+            else
+                console.log(result)
+        })
+    })
+}
+
+printTimes(sample)
+
+//Section 2
