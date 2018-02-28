6f3f788f78729f049495138c39c9df94853892c6
kevinlam825 <kevin.lam825@gmail.com>
2018-02-27 21:22:31 -0800

    completed assignment

--- Homework/Lab2/Lab2.js
+++ Homework/Lab2/Lab2.js
@@ -14,18 +14,45 @@ let result = ''
 let digestResult = ''
 let nonce = 1
 
-
 while (!(digestResult[0] == 0 && digestResult[1] == 0 && digestResult[2] == 0 && digestResult[3] != 0)) { //Loop until we get only 3 leading 0's
     result = message + nonce //set new message to hash
     nonce++ //increment after
     const hash = crypto.createHash(hashingAlgorithm)
     hash.update(result)
-    const digest = hash.digest('hex')
-    digestResult = digest
+    const digest = hash.digest('hex') //get hash
+    digestResult = digest //set hash to check if match found
 }
-//console.log(`The '${hashingAlgorithm}' digest of '${result}' is: ${digestResult}`)
+console.log(`The '${hashingAlgorithm}' digest of '${result}' is: ${digestResult}`) //Print after match is found
+console.log()
 
 //Problem 2
 
+let request = require('request');
 const fs = require('fs')
 const path = require('path')
+const fullPrivateKeyPath = path.resolve('keys', 'private_key.pem')
+const privateKey = fs.readFileSync(fullPrivateKeyPath, 'utf8')
+const sign = crypto.createSign('SHA256')
+const fullPublicKeyPath = path.resolve('keys', 'public_key.pem')
+const publicKey = fs.readFileSync(fullPublicKeyPath, 'utf8')
+
+
+request('http://albertcervantes.com/cs4220/messages.json', (error, response, body) => {
+    let responseObj = JSON.parse(body)
+    responseObj.forEach(function(element) {
+        validateKeys(element)
+    })
+})
+
+let validateKeys = (element) => {
+    const sign = crypto.createSign('SHA256')
+    sign.update(element.message)
+    const signature = sign.sign(privateKey, 'hex')
+
+    verify = crypto.createVerify('SHA256')
+    verify.update(element.message)
+
+    const isValidSignature = verify.verify(publicKey, element.signature, 'hex')
+
+    console.log(`${isValidSignature} - ${element.message}`)
+}
